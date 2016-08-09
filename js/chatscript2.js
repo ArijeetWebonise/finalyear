08081749226c56fec6b12e19e98e8aee8a320bd4
Arijeet <arijeetbaruah@protonmail.ch>
2016-08-09 18:59:48 +0530

    npm add

---
+++ js/chatscript2.js
@@ -0,0 +1,43 @@
+var bot = new RiveScript();
+
+// Load a directory full of RiveScript documents (.rive files). This is for
+// Node.JS only: it doesn't work on the web!
+// bot.loadDirectory("brain", loading_done, loading_error);
+
+
+// Load a list of files all at once (the best alternative to loadDirectory
+// for the web!)
+bot.loadFile([
+    "brain/first.rive"
+], loading_done, loading_error);
+
+// All file loading operations are asynchronous, so you need handlers
+// to catch when they've finished. If you use loadDirectory (or loadFile
+// with multiple file names), the success function is called only when ALL
+// the files have finished loading.
+function loading_done (batch_num) {
+    $("#chatview").html(" ");
+
+    ai("Hello, bot!");
+}
+
+// It's good to catch errors too!
+function loading_error (error) {
+    console.log("Error when loading files: " + error);
+}
+
+function ai(inp){
+    // Now the replies must be sorted!
+    bot.sortReplies();
+    console.log(inp);
+    // And now we're free to get a reply from the brain!
+    var reply = bot.reply("local-user", inp);
+    $("#chatview").append("<div id='chatarea' class='row reply'><b>Anchit</b>: " + reply+"</div>");
+}
+$(document).ready(function(){
+    $(".chatinp").change(function(e){
+        $("#chatview").append("<div id='chatarea' class='row sender'><b>Me</b>: " + $(".chatinp").val()+"</div>");       
+        ai($(".chatinp").val());
+        $(".chatinp").val("");
+    });
+});
